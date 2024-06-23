7e95085d002aa0a868c307e123106672e50f5652
mickoymouse <mckslvstr@gmail.com>
2024-06-23 15:37:11 +0800

    Initial commit and general code cleanup and setup.

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const font = Roboto({
+	weight: ["100", "300", "400", "500", "700", "900"],
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Mardown",
+	description: "Your personal markdown editor",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={font.className}>{children}</body>
+		</html>
+	);
+}
