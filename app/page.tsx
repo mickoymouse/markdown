4cf134bbb745ff5847f040e9c30302b994228ad9
mickoymouse <mckslvstr@gmail.com>
2024-06-23 18:02:45 +0800

    Modify approach to just use local state instead for the data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import { Roboto_Mono, Roboto_Slab } from "next/font/google";
+import { useEffect, useState } from "react";
+
+import { Data, data as initialData } from "@/db/data";
 
 const fontRbSlab = Roboto_Slab({
 	weight: ["100", "300", "400", "500", "700", "900"],
@@ -11,9 +16,30 @@ const fontRbMono = Roboto_Mono({
 });
 
 export default function Home() {
+	const [mdData, setMdData] = useState<Data[]>(initialData);
+
+	useEffect(() => {
+		const data = getFromLS();
+		if (data) {
+			setMdData(JSON.parse(data));
+		} else {
+			saveToLS(initialData);
+			setMdData(initialData);
+		}
+	}, []);
+
+	const saveToLS = (data: Data[]) => {
+		localStorage.setItem("markdown", JSON.stringify(data));
+	};
+
+	const getFromLS = () => {
+		return localStorage.getItem("markdown") ?? null;
+	};
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			Hello there
+		<main className="flex flex-wrap min-h-screen">
+			<div className="w-[50%] bg-black">Markdown Editor</div>
+			<div className="w-[50%]">Preview</div>
 		</main>
 	);
 }
